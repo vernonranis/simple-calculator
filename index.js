9d3c8ada93f267cb0c5ed2e92c77cdd6f1e9458b
vernonranis <vernonranis@gmail.com>
2021-08-21 08:53:24 +0800

    separated the input function

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ let userInput = [];
 
 // TODO place all userInput in an array after user pressed an operator
 // and display it in the display operation input
+// TODO add commas every hundreds increment
 document.addEventListener("keydown", (e) => {
+  inputFn(e);
+});
 
+function inputFn (e) {
   isFinite(e.key) ? userInput += e.key : null;
   console.log(userInput);
   inputDisplaySelector.setAttribute("value", userInput);
-});
+}
 
 
 document.querySelector(".copyright p").innerHTML = '&copy;' + new Date().getFullYear();
