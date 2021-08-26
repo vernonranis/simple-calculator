6cb1c30b0a107bf7512374370aef523f5ca6e5ec
vernonranis <vernonranis@gmail.com>
2021-08-26 10:32:54 +0800

    solved decimal issue

--- index.js
+++ index.js
@@ -3,33 +3,48 @@ let userInput = [];
 let userInputInteger;
 let afterDecimal = [];
 
-// TODO place all userInput in an array after user pressed an operator
 // and display it in the display operation input
 // TODO add dots for decimal inputs
 document.addEventListener('keydown', (e) => {
-  console.log(e.key);
   inputKey(e);
+  console.log('afterDecimal', afterDecimal);
+  console.log('userInput', userInput);
+  console.log('userInputInteger', userInputInteger);
 });
 
 function inputKey(e) {
-  isFinite(e.key) ? (userInput += e.key) : null;
+  if (afterDecimal.length === 0) {
+    isFinite(e.key) ? (userInput += e.key) : null;
+  }
 
   // this part allows user to enter a number and delete a number.
   // If the length will be equal to zero then the display value will be zero as well.
   // TODO try converting this to a ternary operation if possible
   // ISSUE if space and alt key is pressed the calculator wont work anymore
   if (e.key === 'Backspace') {
-    userInput = userInput.slice(0, -1);
-    userInputInteger = parseFloat(userInput);
-    userInputInteger = userInputInteger.toLocaleString('en', {
-      maximumFractionDigits: 0,
-    });
-    inputDisplaySelector.setAttribute('value', userInputInteger);
+    if (afterDecimal.includes('.')) {
+      afterDecimal = afterDecimal.slice(0, -1);
+      console.log(afterDecimal);
+    } else {
+      userInput = userInput.slice(0, -1);
+      userInputInteger = parseFloat(userInput);
+      userInputInteger = userInputInteger.toLocaleString('en', {
+        maximumFractionDigits: 0,
+      });
+    }
+    inputDisplaySelector.setAttribute('value', userInputInteger + afterDecimal);
     if (userInput.length === 0) {
       inputDisplaySelector.setAttribute('value', 0);
     }
   } else if (e.key === '.') {
-    !afterDecimal.includes('.') ? (afterDecimal += e.key) : null;
+    // !afterDecimal.includes('.') ? (afterDecimal += e.key) : null;
+    if (!afterDecimal.includes('.')) {
+      afterDecimal += e.key;
+      inputDisplaySelector.setAttribute(
+        'value',
+        userInputInteger + afterDecimal
+      );
+    }
   } else {
     if (userInput.length === 0) {
       inputDisplaySelector.setAttribute('value', 0);
@@ -38,13 +53,18 @@ function inputKey(e) {
       userInputInteger = userInputInteger.toLocaleString('en', {
         maximumFractionDigits: 0,
       });
-      afterDecimal.includes('.')
-        ? (afterDecimal += e.key)
-        : inputDisplaySelector.setAttribute('value', userInputInteger);
-      inputDisplaySelector.setAttribute(
-        'value',
-        userInputInteger + afterDecimal
-      );
+      if (afterDecimal.includes('.')) {
+        isFinite(e.key) ? (afterDecimal += e.key) : null;
+        inputDisplaySelector.setAttribute(
+          'value',
+          userInputInteger + afterDecimal
+        );
+      } else {
+        inputDisplaySelector.setAttribute(
+          'value',
+          userInputInteger + afterDecimal
+        );
+      }
     }
   }
 }
